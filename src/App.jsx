60dc689e1cef215a7bc6d926b7281821cbf93b0d
Alberto Velarde <alberto.velarde.b@gmail.com>
2026-03-15 08:18:27 -0500

    Se actualizan datos

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ const initialShipments = [
     incoterm: "CIF", 
     lastEvent: "Arrived at transshipment port",
     lastEventEs: "Rumbo al puerto de destino",
-    progress: 97,
+    progress: 98.5,
     routePoints: [
       { name: "Génova", type: "Puerto de origen", done: true },
       { name: "Panamá", type: "Transbordo", done: true },
